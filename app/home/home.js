ab5c3b4ab7c82c44d30c61f5619be614dc098be8
Pei Cheng <duffy.cheng@gmail.com>
2014-09-04 09:46:33 +0800

    fixed delete function

--- app/home/home.js
+++ app/home/home.js
@@ -7,15 +7,20 @@ angular.module("jsonEditorApp")
 }])
 .controller('homeController', ['$scope', '$http','jeaData', 'jeaForm', 'jsonStorage' , 'jsonData', function($scope, $http, jeaData, jeaForm, jsonStorage, jsonData){
 	
-	jeaData.get().then(function(data){
-		$scope.shopData = jsonStorage.get(); 
-	});
+	getData();
 
 	$scope.shopForm = jeaForm;
 
 	$scope.delete = function(index){
 		jeaData.delete(index);
 		jeaData.saveToStorage();
+		getData();
+	};
+
+	function getData(){
+		jeaData.get().then(function(data){
+			$scope.shopData = jsonStorage.get(); 
+		});
 	};
 
 }])
